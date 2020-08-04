846339aa8d94ae7eccb2b51ca11e023365f061c8
JCamyre <JWcamry03@gmail.com>
2020-08-03 17:19:32 -0700

    Created solution for the Day 15 challenge

---
+++ Day15.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data): 
+    #Complete this method
+        if head:
+            cur_node = head
+            while cur_node.next:
+                cur_node = cur_node.next
+            cur_node.next = Node(data)
+            return head
+        else:
+            return Node(data)
+        
+            
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head); 	  
+
